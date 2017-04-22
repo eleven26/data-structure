5f3ac80fbebadbbbd3054daa279e62930303e559
baiguiren <baiguiren@kollway.com>
2017-04-22 13:09:13 +0800

    linear list

--- common.h
+++ common.h
@@ -6,3 +6,12 @@
 #define DATA_STRUCTURE_COMMON_H
 
 #endif //DATA_STRUCTURE_COMMON_H
+
+typedef int Status;
+
+#define SUCCESS  1
+#define FAIL     0
+#define ALLOW_MEM_FAIL -2
+
+#define TRUE     1
+#define FALSE    0
